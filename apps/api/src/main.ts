8c96d70f22d734a254f658e81357c1cab6f2503e
Brice Duke <bricewduke@gmail.com>
2021-06-29 18:48:37 -0500

    feat(api): add graphql

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,12 @@
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import { buildSchema } from 'type-graphql';
 import { createConnection } from 'typeorm';
 
 import { __prod__ } from '@notreddit/api-constants';
 
 import { Post } from './app/entities/Post';
+import { PingResolver } from './app/resolvers/ping';
 
 try {
   (async function () {
@@ -12,6 +16,21 @@ try {
       synchronize: true,
       logging: __prod__,
       entities: [Post],
+    });
+
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+      schema: await buildSchema({
+        resolvers: [PingResolver],
+        validate: false,
+      }),
+    });
+
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => {
+      console.log(`API > Started on http://localhost:4000/graphql`);
     });
   })();
 } catch (err) {
