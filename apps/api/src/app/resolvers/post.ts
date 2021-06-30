310089a7cffc5076e0b0865e4a51016eda89b880
Brice Duke <bricewduke@gmail.com>
2021-06-29 21:39:24 -0500

    feat(post): finish post crud

--- apps/api/src/app/resolvers/post.ts
+++ apps/api/src/app/resolvers/post.ts
@@ -1,11 +1,53 @@
-import { Query, Resolver } from 'type-graphql';
+import { Arg, Int, Mutation, Query, Resolver } from 'type-graphql';
 
 import { Post } from '../entities/Post';
 
 @Resolver()
 export class PostResolver {
+  // Get Posts
   @Query(() => [Post])
   posts(): Promise<Post[]> {
     return Post.find();
+  }
+
+  // Get Post
+  @Query(() => Post, { nullable: true })
+  post(@Arg('id', () => Int) id: number): Promise<Post | undefined> {
+    return Post.findOne(id);
+  }
+
+  // Create Post
+  @Mutation(() => Post)
+  createPost(@Arg('content', () => String) content: string): Promise<Post> {
+    return Post.create({
+      content,
+    }).save();
+  }
+
+  // Update Post
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg('id', () => Int) id: number,
+    @Arg('content', () => String) content: string
+  ): Promise<Post | null> {
+    const post = await Post.findOne(id);
+    if (!post) return null;
+
+    if (typeof content !== 'undefined') {
+      post.content = content;
+      await post.save();
+    }
+    return post;
+  }
+
+  // Update Post
+  @Mutation(() => Boolean)
+  async deletePost(@Arg('id', () => Int) id: number): Promise<boolean> {
+    try {
+      await Post.delete(id);
+      return true;
+    } catch (err) {
+      return false;
+    }
   }
 }
