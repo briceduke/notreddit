8cacc2f6e2ec8cca9691c85079c38b4bb7bba9e0
Brice Duke <bricewduke@gmail.com>
2021-06-30 11:42:38 -0500

    test(input-field): add tests

--- libs/web/web-shared/src/lib/input-field/input-field.spec.tsx
+++ libs/web/web-shared/src/lib/input-field/input-field.spec.tsx
@@ -1,10 +1,23 @@
+import { Formik, Form } from "formik";
 import { render } from '@testing-library/react';
 
 import InputField from '.';
 
 describe('InputField', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<InputField name="test" placeholder="test" label="test" />);
+    const { baseElement } = render(<Formik
+      initialValues={{ username: '', password: '' }}
+      onSubmit={() => {
+        //
+      }}
+    >
+      {() => (
+        <Form>
+          <InputField name="test" placeholder="test" label="test" />
+        </Form>
+      )}
+    </Formik>);
+
     expect(baseElement).toBeTruthy();
   });
 });
